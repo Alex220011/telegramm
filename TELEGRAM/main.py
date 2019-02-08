550ddbcc6cff2d04bc608b3f2a93af38e63f7d23
HappySouS <souspremium@gmail.com>
2019-02-08 19:22:22 +0500

    Проверка

--- TELEGRAM/main.py
+++ TELEGRAM/main.py
@@ -1,11 +1,13 @@
 ﻿import telebot
-import constants
+import constants , datetime
 
 bot = telebot.TeleBot(constants.token)
 
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_msg(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, str(message.datetime))
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
